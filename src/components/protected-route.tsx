02bd23d4a0bb23c0358088235b2a6ef538f1e99d
kimseulgi <ksk177985@gmail.com>
2023-11-11 16:23:53 +0900

    finish

--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -1,15 +1,14 @@
 import { Navigate } from "react-router-dom";
 import { auth } from "../firebase";
-import Home from "../routes/home";
 
 export default function ProtectedRoute({
-        children,
-    }: {
-        children: React.ReactNode;
-    }){
-        const user = auth.currentUser;
-        if(user === null){
-            return <Navigate to="/login" />
-        }
-        return <Home />;
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = auth.currentUser;
+  if (user === null) {
+    return <Navigate to="/login" />;
+  }
+  return children;
 }
